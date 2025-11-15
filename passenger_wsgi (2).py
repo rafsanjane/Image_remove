3df665dd4ebcdb63254f0eae27b1edcedd588f9c
Rafsan Jane <rafsanhoby@gmail.com>
2025-11-15 07:55:49 +0600

    Delete passenger_wsgi (2).py

--- passenger_wsgi (2).py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Make sure this directory is on sys.path
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Import the Flask app from app.py
-from app import app as application
